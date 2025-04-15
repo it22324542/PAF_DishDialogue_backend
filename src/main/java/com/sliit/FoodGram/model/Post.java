9e7b983e227553f69dfdc21973b151d0c7447782
Vishva Prabathiya <vishvaprabathiya2002@gmail.com>
2025-04-16 05:17:25 +0530

    commit2
    create model

---
+++ src/main/java/com/sliit/FoodGram/model/Post.java
@@ -0,0 +1,34 @@
+
+
+
+package com.sliit.FoodGram.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Document(collection = "posts")
+public class Post {
+    // Getters and setters
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private String userId;
+    private String username;
+    private String createdAt;
+    private int likes;
+    private List<String> ingredients;
+    private List<String> steps;
+    private List<String> tags;
+    private String cookTime;
+    private String difficulty;
+
+}
