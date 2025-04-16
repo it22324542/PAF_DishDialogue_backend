c06239006328bd498b9266865ecb8e980730c516
Vishva Prabathiya <vishvaprabathiya2002@gmail.com>
2025-04-16 06:05:30 +0530

    coomit3
    creae controller, service(post)

---
+++ src/main/java/com/sliit/FoodGram/controller/PostController.java
@@ -0,0 +1,54 @@
+
+
+package com.sliit.FoodGram.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sliit.FoodGram.model.Post;
+import com.sliit.FoodGram.service.PostService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequestMapping("/api/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+
+    @GetMapping
+    public List<Post> getPosts() {
+        return postService.getPosts();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPost(@PathVariable String id) {
+        return postService.getPost(id).orElseThrow(() -> new RuntimeException("Post not found"));
+    }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@RequestBody Post post, @PathVariable String id) {
+        return postService.updatePost(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable String id) {
+        postService.deletePost(id);
+    }
+}
